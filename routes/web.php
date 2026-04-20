ee58247be0c3da9e63995f63535aff93e5b2508d
monsef benhar <mouncifbenhar7@gmail.com>
2026-04-20 10:04:20 +0100

    feat: add dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/Dashboard', function () {
+    return view('Dashboard');
+})->middleware('auth.check');
+

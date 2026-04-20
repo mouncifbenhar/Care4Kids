fd017fe9c23590b27b1a9ff15bdd970c7ea5f511
monsef benhar <mouncifbenhar7@gmail.com>
2026-04-20 09:50:00 +0100

    feat: controller authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function Show_register_page()
+    {
+        return view('Auth.register');
+    }
+   
+
+}

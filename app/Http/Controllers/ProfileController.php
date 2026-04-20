313f8d78b5a4f5f839315e0db175a2b8bd980f04
monsef benhar <mouncifbenhar7@gmail.com>
2026-04-20 10:09:59 +0100

    feat: add update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,5 +11,18 @@ class ProfileController extends Controller
     public function profile(){
           return view('profile');
     }
-    
+      public function update_profile(Request $request){
+        $user = Auth::user();
+          $Validated = $request->validate([
+              'name' => 'required|string|max:10',
+              'password' => 'nullable|min:8'
+          ]);
+          
+        $user->name = $Validated['name'];
+        if(!empty($Validated['password'])){
+            $user->password = Hash::make($Validated['password']);
+        }
+        $user->save();
+        return redirect()->back()->with('success', 'Profile updated successfully');
+    }
 }

c88168033b497af9b0ed8dd104ff7eb44f8568e8
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 09:56:52 +0100

    feat: function show midcalrecordar_page

--- app/Http/Controllers/MedicalRecordController.php
+++ app/Http/Controllers/MedicalRecordController.php
@@ -7,5 +7,17 @@ use Illuminate\Http\Request;
 
 class MedicalRecordController extends Controller 
 {
-   
+    public function SHow_MedicalRecord_page($kid){
+
+
+    $child = auth()->user()->kids()->findOrFail($kid);
+
+    $currentAge = $child->birth_date->diffInMonths(now()); 
+
+    $doneVaccinesIds = $child->vaccines()->pluck('vaccines.id');
+
+    $validVaccines = Vaccine::where('target_age_months','<=',$currentAge)->whereNotIn('id', $doneVaccinesIds)->get();
+        return view('Medical_Folder',compact('validVaccines','child'));
+    }
+
 }

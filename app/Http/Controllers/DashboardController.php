b4d23aac71586833e2fabe099b6b12493ca8f94e
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 09:40:54 +0100

    feat: dashboard function to return ('kids','kid_count','medications','med_count','appointments','app_count') of auth user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,5 +8,27 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function Dashboard(){
     
+   
+    $kids = auth()->user()->kids()->get();
+    $kid_count = $kids->count();
+
+    
+    $medications = Medication::whereIn('kid_id', $kids->pluck('id'))->get();
+    $appointments = Appointment::whereIn('kid_id', $kids->pluck('id'))->get();
+
+    $med_count = $medications->count();
+    $app_count = $appointments->count();
+
+   
+
+    return view('Dashboard',compact(
+        
+        'kids','kid_count',
+        'medications', 'med_count',
+        'appointments', 'app_count'
+        
+        ));
+    }
 }

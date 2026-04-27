1d1fa171c659fc4f7f472e29d4d585e95422c5e5
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 10:03:13 +0100

    feat: fillable medicarecord

--- app/Models/MedicalRecord.php
+++ app/Models/MedicalRecord.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicalRecord extends Model
 {
+    protected $fillable = [
+        'kid_id',
+        'weight' ,
+        'height', 
+        'notes',
+        'diagnosis'
+    ];
+
     
 }

4e4f23398b62eda99c099f046c1d49a10f68d75f
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 09:50:31 +0100

    feat:dashbard Route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KidsController;
+use App\Http\Controllers\MedicalRecordController;
+use App\Http\Controllers\MedicationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VaccinesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/Dashboard', function () {
-    return view('Dashboard');
-})->middleware('auth.check');
 
 Route::get('/register',[AuthController::class,'Show_register_page']);
 Route::post('/register',[AuthController::class,'register']);
@@ -20,3 +23,8 @@ Route::post('/login',[AuthController::class,'login']);
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth.check');
 Route::get('/profile',[ProfileController::class,'profile'])->middleware('auth.check');
 Route::post('/update_profile',[ProfileController::class,'update_profile'])->middleware('auth.check');
+
+
+
+Route::get('/Dashboard',[DashboardController::class,'Dashboard'])->middleware('auth.check');
+

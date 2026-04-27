ccdc28eee8724009875af7318813f4e82500e00a
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 11:06:17 +0100

    feat: take_vaccine

--- app/Http/Controllers/VaccinesController.php
+++ app/Http/Controllers/VaccinesController.php
@@ -8,4 +8,21 @@ use Symfony\Component\HttpFoundation\Request;
 
 class VaccinesController extends Controller
 {
+   public function take_vaccine(Request $request, $child) {
+
+    if (!$request->vaccine_id) {
+        return redirect()->back()->with('error_vaccine', 'you have to select vaccine');
+    }
+
+    $child = auth()->user()->kids()->findOrFail($child);
+
+    $vaccine = Vaccine::findOrFail($request->vaccine_id);
+
+    $child->vaccines()->syncWithoutDetaching($request->vaccine_id); 
+
+    
+    auth()->user()->notify(new VaccineReminder($vaccine, $child->name));
+    
+    return redirect()->back()->with('success_vaccine', 'vaccine added successfully');
+}
 }

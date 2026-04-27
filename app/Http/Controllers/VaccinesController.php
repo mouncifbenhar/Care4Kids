daf0898a3cb2816350736a27abf131d119397ce8
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 11:05:12 +0100

    feat: VaccinesController

---
+++ app/Http/Controllers/VaccinesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vaccine;
+use App\Notifications\VaccineReminder;
+use Symfony\Component\HttpFoundation\Request;
+
+class VaccinesController extends Controller
+{
+}

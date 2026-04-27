5ea0577fdbfc771cdc700f828d1ef0d0a974bd58
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 11:04:10 +0100

    feat: function add_appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -11,4 +11,21 @@ class AppointmentController extends Controller
          $child = auth()->user()->kids()->findOrFail($kid);
          return view('Appointment',compact('child'));
     }
+    public function add_appointment(Request $request, $child)
+{
+   
+    $validated = $request->validate([
+        'doctor_name'      => 'nullable|string|max:255',
+        'specialty'        => 'nullable|string|max:100',
+        'appointment_date' => 'required|date|after:now', 
+        'location'         => 'nullable|string|max:255',
+        'reason'           => 'nullable|string|max:1000',
+        'notes'            => 'nullable|string|max:1000'
+    ]);
+
+    $child = auth()->user()->kids()->findOrFail($child);
+    $child->appointments()->create($validated);
+   
+    return back()->with('success_appointment', ' added Appointments successfully');
+}
 }

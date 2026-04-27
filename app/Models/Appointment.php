7df25a830286b9a8f984746a574665791c56a756
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 11:08:19 +0100

    feat: fillable , function belong to

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,4 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
+    protected $fillable = [
+        'kid_id',
+        'doctor_name',
+        'specialty',
+        'appointment_date',
+        'location',
+        'reason',
+        'notes',
+        'is_completed'
+    ];
+    public function kid()
+    { 
+    return $this->belongsTo(Kid::class,'kid_id');
+    }
 }

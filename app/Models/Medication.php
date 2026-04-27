264753c937d5fff683d5d0ae212b83adaecad698
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 10:21:06 +0100

    feat: fillable mediaction

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medication extends Model
 {
+  protected $fillable = [
+    'kid_id',
+    'name',
+    'dosage',
+    'first_dose_time',
+    'interval_hours',
+    'start_date',
+    'end_date',
+    'frequency',
+    'notes',
+];
+
 
 
 }

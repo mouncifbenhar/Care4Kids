ad10c272eb0514e443f6950ef9df47e109b2522f
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 09:39:05 +0100

    feat: dachbord controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointment;
+use App\Models\Medication;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    
+}

542d526473f50b6e40f55ec6bbc8f5348ff05748
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 11:10:11 +0100

    feat: fillable , function belong to many

--- app/Models/Vaccine.php
+++ app/Models/Vaccine.php
@@ -6,4 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vaccine extends Model
 {
+    protected $fillable = [
+        'name', 
+        'target_age_months', 
+        'description'
+        ];
+    
+        public function Kids(){
+            return $this->belongsToMany(Kid::class, 'kid_vaccine')->withTimestamps();
+        }
+    
 }

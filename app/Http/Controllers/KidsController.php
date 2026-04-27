d665886f40bcaaa24636205b1efe5a8b2f71899d
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 09:46:29 +0100

    feat: function add_kid for create new kid

--- app/Http/Controllers/KidsController.php
+++ app/Http/Controllers/KidsController.php
@@ -10,5 +10,23 @@ class KidsController extends Controller
     public function show_kids_page(){
         return view('Kids');
     }
+    public function add_kid(Request $request){
+    $validated = $request->validate([
+               'name' => 'required|string|max:255',
+               'gender' => 'required|in:female,male', 
+               'birth_date' => 'required|date|before:today', 
+               'height' => 'required|numeric|min:15|max:200', 
+               'weight' => 'required|numeric|min:1|max:150',  
+               'blood_type' => 'required|in:A+,O+,B+,AB+,A-,O-,B-,AB-', 
+               'has_special_case' => 'boolean',
+               'special_case_details' => 'required_if:has_special_case,1|nullable|string|min:5', 
+
+    ]);
+    $validated['user_id'] = auth()->id();
+    Kid::create($validated);
+    return redirect()->back()->with('success', 'You hade add your kid successfully');
+    }
+    //--------------------------------------------------------------------------------------
+
    
 }

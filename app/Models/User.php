cd0bfafd1c23052430e6654753c7e45c4e689de2
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-04-27 10:15:56 +0100

    feat: function kids user hasMany kid

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -28,5 +26,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function kids()
+    {
+        return $this->hasMany(Kid::class);
     }
 }
